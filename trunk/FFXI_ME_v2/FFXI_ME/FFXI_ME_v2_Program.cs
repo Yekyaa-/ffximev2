0090e8fa628b9537f051c2bd43323cfcc1afd7da
doomer18@gmail.com <doomer18@gmail.com@12de8481-91a6-a431-f221-3fbfc630e094>
2010-07-01 18:17:58 +0000

    Added checks to avoid subfolders and folders being present at the same time

--- trunk/FFXI_ME_v2/FFXI_ME/FFXI_ME_v2_Program.cs
+++ trunk/FFXI_ME_v2/FFXI_ME/FFXI_ME_v2_Program.cs
@@ -12,6 +12,13 @@ namespace FFXI_ME_v2
 {
     static class FFXI_ME_v2_Program
     {
+        public static bool Deleteable(String x)
+        {
+            if (x == "<x_ffxime_x> Delete Me")
+                return true;
+            return false;
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -19,9 +26,12 @@ namespace FFXI_ME_v2
         static void Main(string[] args)
         {
             Preferences.PathToOpen.Clear();
+            bool Delete = false, Skip = false;
 
             for (int i = 0; i < args.Length; i++)
             {
+                Delete = false;
+                Skip = false;
                 if ((args[i] == "/debug") || (args[i] == "-debug"))
                     Preferences.ShowDebugInfo = true;
                 else if ((args[i] == "/options") || (args[i] == "-options") || 
@@ -29,8 +39,41 @@ namespace FFXI_ME_v2
                     MainForm.ShowOptionsDialog = true;
                 else if (args[i] != String.Empty)
                 {
-                    if (!Preferences.PathToOpen.Contains(args[i]))
-                        Preferences.PathToOpen.Add(args[i]);
+                    DirectoryInfo di = new DirectoryInfo(args[i]);
+                    if (Preferences.PathToOpen.Contains(di.FullName)) // if we already have this exact folder, skip it
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        for (int c = 0; c < Preferences.PathToOpen.Count; c++)
+                        {
+                            if (Preferences.PathToOpen[c].Contains(di.FullName))
+                            {
+                                // if args[i] is a Parent Folder of an existing Path
+                                Preferences.PathToOpen[c] = "<x_ffxime_x> Delete Me";
+                                Delete = true;
+                            }
+                            else if (di.FullName.Contains(Preferences.PathToOpen[c]))
+                            {
+                                Skip = true;
+                            }
+                        }
+                        if (Delete)
+                        {
+                            Preferences.PathToOpen.RemoveAll(Deleteable);
+                            //while (Preferences.PathToOpen.Contains("<x_ffxime_x> Delete Me"))
+                            //{
+                            //    Preferences.PathToOpen.Remove("<x_ffxime_x> Delete Me");
+                            //}
+                            
+                        }
+                        if (Skip)
+                            continue;
+
+                        if (!Preferences.PathToOpen.Contains(di.FullName))
+                            Preferences.PathToOpen.Add(di.FullName);
+                    }
                 }
             }
 
